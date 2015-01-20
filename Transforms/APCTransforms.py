c0cf5df1f5b58cf15862f930664897f5d9f6e8bc
Lincoln McFarland <Lincoln.McFarland@rsa.com>
2015-01-20 15:09:19 -0800

    adds APC MiniSun method

--- Transforms/APCTransforms.py
+++ Transforms/APCTransforms.py
@@ -9,6 +9,8 @@
 import math
 import coords
 
+import Transforms
+
 class APCTransforms(object):
 
     horizon_axis = coords.rotator(coords.Uy)
@@ -119,7 +121,30 @@ class APCTransforms(object):
         """Calculates the Sun's RA and declination
 
 
-        from
+        from p. 39
 
+
+        TODO generating data for an analemma:
+
+        maximum altitude (minimum colatitude) is 2015-07-21 not 2015-06-21
+
+        colatitude snaps from 70 to 79 on 2015-01-04.
+        azimuth snaps from 178 to 97 on 2015-01-04.
+        L snaps from 4.9 to 0.01 at the same time. Frac?
 
         """
+
+        T = Transforms.Transforms.JulianCentury(a_datetime)
+        eps = coords.angle(Transforms.EclipticEquatorial.eps(a_datetime))
+
+        M = 2*math.pi * cls.Frac(0.993133 + 99.997361*T) # Mean anomaly
+        L = 2*math.pi * cls.Frac(0.7859453 * M/(2*math.pi) + (6893.0*math.sin(M) + 72.0*math.sin(2.0*M) + 6191.2*T)/1296.0e3)
+
+        sun_azimuth = coords.angle()
+        sun_azimuth.radians = L
+
+        sun = coords.spherical(1, coords.angle(90), sun_azimuth)
+
+        sun_eq = Transforms.EclipticEquatorial.toEquatorial(sun, a_datetime)
+
+        return sun_eq
--- Transforms/test_APCTransforms.py
+++ Transforms/test_APCTransforms.py
@@ -218,6 +218,32 @@ class APCTransformTests(unittest.TestCase):
         self.assertAlmostEqual(45, hz_point.theta.value)
         self.assertAlmostEqual(0, hz_point.phi.value)
 
+    @unittest.skip('TODO')
+    def test_analemma(self):
+        """Test mini sun
+
+        TODO generating data for an analemma:
+
+        maximum altitude (minimum colatitude) is July 21, not June 21
+
+        azimuth snaps from 178 to 97 on 2015-01-04.
+        L snaps from 4.9 to 0.01 at the same time
+        """
+
+        an_observer = coords.spherical(1, coords.latitude(37, 24), coords.angle(-122, 4, 57))
+
+        for m in xrange(1, 13):
+            for d in xrange(1, 28): # skips some days
+
+                a_datetime = coords.datetime('2015-%02d-%02dT12:00:00' % (m, d))
+
+                print a_datetime,  # TODO rm
+
+                sun_eq = self.xforms.MiniSun(a_datetime)
+
+                sun_hz = Transforms.EquatorialHorizon.toHorizon(sun_eq, an_observer, a_datetime)
+
+                print 'colatitude', sun_hz.theta, 'azimuth', sun_hz.phi # TODO rm
 
 
 if __name__ == '__main__':
