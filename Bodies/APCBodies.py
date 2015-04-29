7ae21a30fd1c8d003680c7c3c4f082dc5ab18c38
Lincoln McFarland <Lincoln.McFarland@rsa.com>
2015-04-28 17:29:53 -0700

    adds first cut at APC mini moon

--- Bodies/APCBodies.py
+++ Bodies/APCBodies.py
@@ -57,6 +57,51 @@ def MiniSun(a_datetime):
     return sun_ec
 
 
+def MiniMoon(a_datetime):
+    """Calculates the moon's RA and declination for the given datetime.
+
+    from APC pp. 38-39
+
+    Returns the position of the sun in ecliptic coordinates (coords.spherical)
+    """
+
+    T = utils.JulianCentury(a_datetime)
+    eps = coords.angle(EclipticEquatorial.eps(a_datetime))
+
+    Lo = Frac(0.606433 + 1336.855225 * T) # mean longitude
+
+    lm = 2*math.pi * Frac(0.374897 + 1325.55241  * T) # Moon's mean anomaly
+    ls = 2*math.pi * Frac(0.993133 +   99.997361 * T) # Sun's mean anomaly
+    D  = 2*math.pi * Frac(0.827361 + 1236.853086 * T) # Diff. long. Moon - Sun
+    F  = 2*math.pi * Frac(0.259086 + 1342.227825 * T) # Distance from ascending node
+
+    # Perturbations in longitude and latitude
+
+    dL = + 22640*math.sin(lm) - 4586*math.sin(lm - 2*D) + 2370*math.sin(2*D) + 769*math.sin(2*lm) \
+         - 668*math.sin(ls) - 412*math.sin(2*F) - 212*math.sin(2*lm - 2*D) - 206*math.sin(lm + ls - 2*D) \
+         + 192*math.sin(lm + 2*D) - 165*math.sin(ls - 2*D) - 125*math.sin(D) - 110*math.sin(lm + ls) \
+         + 148*math.sin(lm - ls) - 55*math.sin(2*F - 2*D)
+
+
+    latitude = 2*math.pi * Frac(Lo + dL/1296.0e3) # radians
+
+
+    print 'lunar latitude?', coords.angle().rad2deg(latitude)
+
+
+    Arcs = 3600 # TODO find this
+
+    S = F + (dL + 412*math.sin(2*F) + 541*math.sin(ls)) / Arcs
+    h = F - 2*D
+    N = -526*math.sin(h) + 44*math.sin(lm + h) - 31*math.sin(-lm + h) - 23*math.sin(ls + h) \
+        + 11*math.sin(-ls + h) - 25*math.sin(-2*lm + F) + 21*math.sin(-lm + F)
+
+
+    longitude = (18520.0*math.sin(S) + N) / Arcs
+
+    print 'lunar longitude?', coords.angle().rad2deg(longitude)
+
+
 # ================
 # ===== main =====
 # ================
@@ -70,11 +115,20 @@ if __name__ == '__main__':
 
     import optparse
 
-    defaults = {'isVerbose': False}
+    bodies = ('sun', 'moon')
+
+    defaults = {'isVerbose': False,
+                'body': 'sun'}
 
     usage = '%prog [options] <latitude> <longitude> <datetime>'
 
     parser = optparse.OptionParser(usage=usage)
+
+    parser.add_option('-b', '--body',
+                      action='store', dest='body',
+                      type='choice', choices=bodies,
+                      default=defaults['body'],
+                      help='body %s' % (bodies,))
 
     parser.add_option('-v', '--verbose',
                       action='store_true', dest='verbose',
@@ -93,24 +147,36 @@ if __name__ == '__main__':
 
     a_datetime = coords.datetime(args[2])
 
-    # ----------------------------------
-    # ----- calculate sun position -----
-    # ----------------------------------
+    # ------------------------------
+    # ----- calculate position -----
+    # ------------------------------
 
     print 'A datetime: ', a_datetime
     print 'An observer:', an_observer
 
-    sun_ec = MiniSun(a_datetime)
-    print 'Sun in ecliptic coordinates:\n\t', sun_ec
+    if options.body.lower() == 'sun':
 
-    sun_eq = EclipticEquatorial.toEquatorial(sun_ec, a_datetime)
-    print 'Sun in equatorial coordinates:\n\t', sun_eq
+        sun_ec = MiniSun(a_datetime)
+        print 'Sun in ecliptic coordinates:\n\t', sun_ec
+
+        sun_eq = EclipticEquatorial.toEquatorial(sun_ec, a_datetime)
+        print 'Sun in equatorial coordinates:\n\t', sun_eq
+
+        sun_hz = EquatorialHorizon.toHorizon(sun_eq, an_observer, a_datetime)
+        print 'Sun in horizon coordinates:\n\t', sun_hz
+
+        print 'Azimuth (degrees):', utils.get_azimuth(sun_hz),
+        print ''.join(('(', str(utils.get_azimuth(sun_hz).value), ')'))
+        print 'Altitude (degrees):', utils.get_altitude(sun_hz),
+        print ''.join(('(', str(utils.get_altitude(sun_hz).value), ')'))
+
+    elif options.body.lower() == 'moon':
 
 
-    sun_hz = EquatorialHorizon.toHorizon(sun_eq, an_observer, a_datetime)
-    print 'Sun in horizon coordinates:\n\t', sun_hz
+        moon_ec = MiniMoon(a_datetime)
+        print 'Moon in ecliptic coordinates:\n\t', moon_ec
 
-    print 'Azimuth (degrees):', utils.get_azimuth(sun_hz),
-    print ''.join(('(', str(utils.get_azimuth(sun_hz).value), ')'))
-    print 'Altitude (degrees):', utils.get_altitude(sun_hz),
-    print ''.join(('(', str(utils.get_altitude(sun_hz).value), ')'))
+
+    else:
+
+        print 'unknown option'
