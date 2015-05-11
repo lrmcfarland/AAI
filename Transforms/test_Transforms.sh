a6cea17a293a6730a5f09f8213ff109198029086
Lincoln McFarland <Lincoln.McFarland@rsa.com>
2015-05-11 16:02:28 -0700

    more verbose test wrapper

--- Transforms/test_Transforms.sh
+++ Transforms/test_Transforms.sh
@@ -2,8 +2,27 @@
 
 . ./setenv.sh
 
+echo '============='
+echo 'APC'
+echo '============='
 python test_APCTransforms.py "$@"
+
+echo '============'
+echo 'Sun Position'
+echo '============'
+
+echo 'Ecliptic Equatorial'
 python test_EclipticEquatorial.py "$@"
+echo '- - - - - - '
+
+echo 'Equatorial Horizon'
 python test_EquatorialHorizon.py "$@"
+echo '- - - - - - '
+
+echo 'GMST'
 python test_GMST.py "$@"
+echo '- - - - - - '
+
+echo 'Utils'
 python test_utils.py "$@"
+echo '- - - - - - '
