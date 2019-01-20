8159dd93f73d449d0cc24676abc4c4176c42d185
L.R. McFarland <lrm@starbug.com>
2019-01-19 22:46:10 -0800

    simplifies flask config

--- www/config/aai-flask-testing-config.py
+++ www/config/aai-flask-testing-config.py
@@ -4,16 +4,10 @@ import os
 
 DEBUG = True
 
-APPLICATION_ROOT = os.getcwd()
-SESSION_COOKIE_PATH = os.path.join(os.getcwd(), 'config')
-
-SERVER_NAME = '0.0.0.0:8080'
-# TODO https://stackoverflow.com/questions/27254013/why-does-the-session-cookie-work-when-serving-from-a-domain-but-not-when-using-a
-SESSION_COOKIE_DOMAIN = SERVER_NAME
-
 SECRET_KEY = 'changeme'
 
-# non-flask available via config['GOOGLEMAPS_KEY'],
+# non-flask config is available via config['GOOGLEMAPS_KEY'],
 # e.g. {{config['GOOGLEMAPS_KEY']}} in templates
+
 GOOGLEMAPS_KEY = 'changeme'
 
